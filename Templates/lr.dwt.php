ea0b7c29c44f22ec6d071b25bf5b2f2242b0485e
design9992 <design9992@gmail.com>
2017-06-27 21:12:24 -0700

    basic structure

---
+++ Templates/lr.dwt.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>html5</title>
+<!-- TemplateEndEditable -->
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="../lr/css/bootstrap.min.css">
+<!-- Latest compiled and minified JavaScript -->
+<script src="../lr/js/bootstrap.min.js"></script>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="../lr/js/jquery.min.js"></script>
+
+
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+
+<div class="container">
+<!-- TemplateBeginEditable name="EditRegion3" -->
+  <div>my basic content</div>
+<!-- TemplateEndEditable -->
+
+
+</div>
+
+</body>
+</html>
